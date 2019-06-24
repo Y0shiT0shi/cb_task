fe5c4c0b3061aa65a5f7da2108ac947f84f7e39a
Sergei Lubimcev <yukian72@gmail.com>
2019-06-24 20:43:26 +0500

    second project version

--- features/environment.py
+++ features/environment.py
@@ -2,4 +2,4 @@ import email_my
 
 
 def after_feature(context, feature):
-    email_my.send_email('yukian72@gmail.com', 'lybimtcev.si@yandex.ru', 'k1s$2as5', 'smtp.gmail.com', 587)
+    email_my.send_email('sender@mail.ru', 'reciever@mail.ru', '123456', 'smtp.gmail.com', 587)
